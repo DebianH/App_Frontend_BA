34edb351054820fe0260cf118632f8e7c98836f5
DavidH4tech <david.hernandez@4tech.com.co>
2024-09-30 22:01:12 -0500

    update drawer 30-09-2024

--- src/components/routesNavigation/routesScreens.tsx
+++ src/components/routesNavigation/routesScreens.tsx
@@ -1,27 +1,29 @@
 // rutasDeScreens.tsx
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Animated, Dimensions, SafeAreaView, Text, StatusBar } from 'react-native';
 import HomeScreenPage from '../Pages/HomeScreenPage';
 import DonationScreenPage from '../Pages/DonationScreenPage';
 import ChartScreenPage from '../Pages/ChartUserScreenPage';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerItemList } from '@react-navigation/drawer';
 import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/Ionicons';
-// import Icons from 'react-native-vector-icons/Carbon';
+import { useRef } from 'react';
 
 import DetailsScreen from '../Pages/NewBAPage';
 import QrScreenPage from '../Pages/QrScreenPage';
 import ProfileHomeScreenPage from '../Pages/ProfileHomeScreenPage';
 import ProfileEditPage from '../Pages/ProfileEditPage';
-// import HomeScreenPage from '../Pages/HomeScreenPage';
-// export type RootStackParamList = {
-//   HomeScreenPage: undefined;
-//   DonationScreenPage: undefined;
-//   ChartScreenPage: undefined;
-//   QrScreenPage: undefined;
-//   DetailsScreen: undefined;
-// };
+
+export type RootStackParamList = {
+  HomeStack: undefined;
+  DonationStack: undefined;
+  ChartStack: undefined;
+  QrStack: undefined;
+  Drawer: undefined;
+  EditProfileStack: undefined;
+  Routes: undefined;
+};
 
 
 const Tab = createBottomTabNavigator();
@@ -34,13 +36,14 @@ const EditProfileStack = createNativeStackNavigator();
 //Drawer
 const Drawer = createDrawerNavigator();
 
+
 function ProfileScreen() {
   return (
     <EditProfileStack.Navigator initialRouteName="HomeProfile">
       <EditProfileStack.Screen
         name="HomeProfile"
         component={ProfileHomeScreenPage}
-        options={{ headerShown: true, }}
+        options={{ headerShown: false }}
       />
       <EditProfileStack.Screen
         name="EditProfile"
@@ -100,7 +103,41 @@ function QrStackScreen() {
 }
 function DrawerGroup() {
   return (
-    <Drawer.Navigator initialRouteName='Routes' screenOptions={{ headerShown: false }}>
+    <Drawer.Navigator
+      initialRouteName={'Routes'}
+      screenOptions={{ headerShown: false, }}
+      drawerContent={(props) => {
+        return (
+          <SafeAreaView style={{ flex: 1 }}>
+            <View style={{
+              // flex: 1,
+              height: 250,
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignContent: 'center',
+              gap: 10,
+              // backgroundColor: 'tomato',
+            }}>
+              <Image
+                source={require('../../assets/avatarMen.png')}
+                style={{ width: 100, height: 100, borderRadius: 50, borderColor: '#67b349', borderWidth: 1 }}
+                resizeMode="contain"
+              />
+              <Text style={{ fontSize: 18, color: '#b1b1b1' }}>Jose Luis</Text>
+              <View style={{ flexDirection: 'row', gap: 10, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ fontSize: 12, color: '#4a4a4a', }}>Beneficiarios</Text>
+                <Text style={{ fontSize: 12, color: '#4a4a4a', fontWeight: "bold" }}>29</Text>
+              </View>
+            </View>
+            <DrawerItemList {...props} />
+            <View >
+              <Text style={{ fontSize: 14, color: '#c0c0c0', textAlign: 'center', marginTop: 300 }}>Rol: Organización</Text>
+            </View>
+          </SafeAreaView>
+        )
+      }}
+    >
       <Drawer.Screen
         name='ProfileNavigation'
         component={ProfileNavigation}
@@ -110,7 +147,7 @@ function DrawerGroup() {
           headerTitleAlign: 'center',
           drawerLabel: 'Mi Perfil',
           drawerIcon: ({ color, size }) => (
-            <Icon name="person-circle-outline" size={28} color={"black"} />
+            <Icon name="person-outline" size={28} color={"black"} />
           ),
         }} />
       <Drawer.Screen name='Routes' component={Routes}
@@ -147,42 +184,73 @@ function DrawerGroup() {
           ),
         }} />
 
+      <Drawer.Screen
+        name='Contributions'
+        component={ProfileNavigation}
+        options={{
+          headerShown: true,
+          headerTitle: 'Contribuciones',
+          headerTitleAlign: 'center',
+          drawerLabel: 'Contribuciones',
+          drawerIcon: ({ color, size }) => (
+            <Icon name="heart-outline" size={28} color={"black"} />
+          ),
+        }} />
+
+      <Drawer.Screen
+        name='Help'
+        component={ProfileNavigation}
+        options={{
+          headerShown: true,
+          headerTitle: 'Ayuda',
+          headerTitleAlign: 'center',
+          drawerLabel: 'Ayuda',
+          drawerIcon: ({ color, size }) => (
+            <Icon name="help-circle-outline" size={28} color={"black"} />
+          ),
+        }} />
     </Drawer.Navigator>
   )
 }
 function Routes() {
+
   return (
     <Tab.Navigator
       initialRouteName='Inicio'
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
         tabBarStyle: {
           height: 70,
           paddingBottom: 10,
           paddingTop: 10,
-          elevation: 10,
+          // elevation: 10,
+          borderTopColor: '#c0c0c0',
+          borderTopWidth: 1,
         },
         tabBarLabelStyle: {
           fontSize: 14,
         },
-      }}>
-      <Tab.Screen name="Check" component={QrStackScreen} options={{
-        tabBarIcon: ({ color, size, focused }) => (
-          <Icon
-            name={focused ? 'qr-code' : 'qr-code-outline'}
-            size={28}
-            color={focused ? '#f39200' : '#c0c0c0'}
-          />
-        ),
-        tabBarActiveTintColor: '#f39200',
-        tabBarInactiveTintColor: '#c0c0c0',
-        // tabBarLabel: 'Check',
-      }} />
+      })}>
+      <Tab.Screen
+        name="Check"
+        component={QrStackScreen}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => (
+            <Icon
+              name={focused ? 'qr-code' : 'qr-code-outline'}
+              size={focused ? 30 : size}
+              color={focused ? '#f39200' : '#c0c0c0'}
+            />
+          ),
+          tabBarActiveTintColor: '#f39200',
+          tabBarInactiveTintColor: '#c0c0c0',
+        }}
+      />
       <Tab.Screen name="Gráficas" component={ChartStackScreen} options={{
         tabBarIcon: ({ color, size, focused }) => (
           <Icon
             name={focused ? 'bar-chart' : 'bar-chart-outline'}
-            size={28}
+            size={focused ? 30 : size}
             color={focused ? '#f39200' : '#c0c0c0'}
           />
         ),
@@ -193,7 +261,7 @@ function Routes() {
         tabBarIcon: ({ color, size, focused }) => (
           <Icon
             name={focused ? 'home' : 'home-outline'}
-            size={size}
+            size={focused ? 30 : size}
             color={color}
           />
         ),
@@ -204,8 +272,9 @@ function Routes() {
         tabBarIcon: ({ color, size, focused }) => (
           <Icon
             name={focused ? 'bag-check' : 'bag-check-outline'}
-            size={28}
+            size={focused ? 30 : size}
             color={focused ? '#f39200' : '#c0c0c0'}
+
           />
         ),
         tabBarActiveTintColor: '#f39200',
